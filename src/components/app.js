f947cbc2d2dc30e6b0f13b6b0fabe0f1ce97222c
Niki <ntubacki@mit.edu>
2018-04-30 14:42:40 -0400

    cleaning

--- src/components/app.js
+++ src/components/app.js
@@ -2,10 +2,8 @@ import React, { Component } from 'react';
 import ProductList   from '../containers/ProductList';
 import ProductDetail from '../containers/ProductDetail';
 import Background from './background.js';
-import { Container } from 'semantic-ui-react';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { DragDropContext } from 'react-dnd';
-// import SwipeCard from '../containers/SwipeCard';
 
 class App extends Component {
 
